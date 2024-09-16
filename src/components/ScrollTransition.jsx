29599707678b702cbc6ca199f455cea612e9c564
Freddy Lopez <freddlopez01@gmail.com>
2024-09-15 19:07:49 -0700

    More edits

--- src/components/ScrollTransition.jsx
+++ src/components/ScrollTransition.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import styles from "./ScrollTransition.css";
-
 const ScrollTransition = () => {
   useEffect(() => {
     const SHITVIDEO = document.querySelector('#video');
